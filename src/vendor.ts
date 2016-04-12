a185bd382d8e543629ad2daf2b6d876753b60806
mshahin364 <mshahin364@gmail.com>
2016-04-12 15:56:12 +0600

    External vendor file added

---
+++ src/vendor.ts
@@ -0,0 +1,20 @@
+// Angular 2
+import 'angular2/platform/browser';
+import 'angular2/platform/common_dom';
+import 'angular2/core';
+import 'angular2/router';
+import 'angular2/http';
+
+// RxJS
+import 'rxjs';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'jquery';
+// import 'bootstrap';
+import 'jquery';
+import 'bootstrap-loader';
+
+import 'lodash';
+import 'font-awesome/css/font-awesome.css';
